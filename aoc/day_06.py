f68693f913e3ff069071e426a79519571c7fdfb1
Jamie Barker <dodgerbarker@gmail.com>
2024-12-16 16:36:16 +0000

    day 6 finally

--- aoc/day_06.py
+++ aoc/day_06.py
@@ -1,3 +1,16 @@
+def walk(G, start):
+
+    pos, v, seen = start, 1j, set()
+
+    while pos in G and (pos, v) not in seen:
+        seen.add((pos, v))
+        if G.get(pos + v) == "#":
+            v *= -1j
+        else:
+            pos += v
+    return {s[0] for s in seen}, (pos, v) in seen
+
+
 def part_one(file_path: str):
     """[summary]
 
@@ -11,25 +24,15 @@ def part_one(file_path: str):
     # read file
     with open(file_path) as f:
         lines = f.readlines()
-    
-    barriers = []
-    for x, line in enumerate(lines[::-1]):
-        for y, char in enumerate(line.strip()):
-            if char == "#":
-                barriers.append(y + x*1j)
-            elif char != ".":
-                pos = y + x*1j
 
-    increment = 0 + 1j
-    locations = set()
-    while min(pos.real, pos.imag) >= 0 and pos.real < x and pos.imag < y:
-        
-        if pos + increment in barriers:
-            increment *= -1j
-        locations.add(pos)
-        pos += increment
+    G = {}
+    for y, line in enumerate(lines[::-1]):
+        for x, char in enumerate(line.strip()):
+            G[x + y * 1j] = char
+            if char == "^":
+                start = x + y * 1j
 
-    return len(locations)
+    return len(walk(G, start)[0])
 
 
 def part_two(file_path: str):
@@ -41,75 +44,20 @@ def part_two(file_path: str):
     Returns:
         [type]: [description]
     """
+    # read file
+    with open(file_path) as f:
+        lines = f.readlines()
+    G = {}
+    for y, line in enumerate(lines[::-1]):
+        for x, char in enumerate(line.strip()):
+            G[x + y * 1j] = char
+            if char == "^":
+                start = x + y * 1j
 
-    return None
-    # with open(file_path) as f:
-    #     lines = f.readlines()
-    # # parse barriers and position
-    # barriers = set()
-    # for x, line in enumerate(lines[::-1]):
-    #     for y, char in enumerate(line.strip()):
-    #         if char == "#":
-    #             barriers.add(y + x*1j)
-    #         elif char != ".":
-    #             pos = y + x*1j
+    path = walk(G, start)[0]
 
-    # ## Get all points on the path
-    # increment = 0 + 1j
-    # positions = set()
-    # while min(pos.real, pos.imag) >= 0 and pos.real < x and pos.imag < y:
-        
-    #     if pos + increment in barriers:
-    #         increment *= -1j
-    #     positions.add(pos)
-    #     pos += increment
+    return sum(walk(G | {barrier: "#"}, start)[1] for barrier in path)
 
-    # # get all positions adjacent to barriers that map to barriers and the position 
-    # # where they would change direction
-    # pos_to_bar = {}
-    # for barrier in barriers:
-    #     for pos_rel in [1j, -1j, 1, -1]:
-    #         pos = barrier + pos_rel
-    #         inc = pos_rel * 1j
-    #         while min(pos.real, pos.imag) >= 0 and pos.real < x and pos.imag < y:
-    #             if pos + inc in barriers:
-    #                 pos_to_bar[barrier + pos_rel] = pos
-    #                 break
-    #             pos += inc
-
-    # for pos in positions:
-
-    
-    
-    # inc = 0 + 1j
-    # positions = set()
-    # loop_barriers = set()
-    # while min(pos.real, pos.imag) >= 0 and pos.real < x and pos.imag < y:
-    #     if pos + inc in barriers:
-    #         inc *= -1j
-    #     positions.add((pos, inc))
-    #     pos += inc
-
-    #     ### Adding simulation of adding barrier in front
-    #     new_barriers = barriers | set([pos + inc])
-    #     new_pos, new_inc = pos + inc*-1j, inc*-1j
-    #     new_positions = positions | set([(new_pos, new_inc)])
-    #     if check_for_loop(new_pos, new_inc, new_positions, new_barriers, x, y):
-    #         loop_barriers.add(pos + inc)
-        
-    # return len(pos_to_bar)
-
-# def check_for_loop(pos, inc, positions, barriers, x, y):
-
-#     while min(pos.real, pos.imag) >= 0 and pos.real < x and pos.imag < y:
-#         if pos + inc in barriers:
-#             inc *= -1j
-#         if (pos +inc, inc) in positions:
-#             return True
-#         positions.add((pos, inc))
-#         pos += inc
-
-#     return False
 
 if __name__ == "__main__":
     print(part_one("aoc/inputs/day_06.txt"))
--- tests/test_day_06.py
+++ tests/test_day_06.py
@@ -1,10 +1,12 @@
 from aoc.day_06 import part_one, part_two
 
+
 def test_part_one():
 
     test_file_path = "tests/test_inputs/test_day_06.txt"
 
     assert part_one(test_file_path) == 41
+
 
 def test_part_two():
 

626eb34bb6c38cee724c2dcaa6a0abf9193b050e
Jamie Barker <dodgerbarker@gmail.com>
2024-12-17 21:59:26 +0000

    day 7

--- aoc/day_07.py
+++ aoc/day_07.py
@@ -1,37 +1,32 @@
 def calculate_equations(start: int, remaining: list[int]) -> list[int]:
 
-    options = set()
+    options = []
 
     if len(remaining) == 1:
-        return set([start + remaining[0], start * remaining[0]])
+        return [start + remaining[0], start * remaining[0]]
 
-    options |= calculate_equations(start + remaining[0], remaining[1:])
-    options |= calculate_equations(start * remaining[0], remaining[1:])
+    options += calculate_equations(start + remaining[0], remaining[1:])
+    options += calculate_equations(start * remaining[0], remaining[1:])
 
     return options
-
 
 def calculate_equations_two(start: int, remaining: list[int]) -> list[int]:
 
-    options = set()
+    options = []
 
     if len(remaining) == 1:
-        return set(
-            [
-                start + remaining[0],
-                start * remaining[0],
-                int("".join([str(start), str(remaining[0])])),
+        return [
+            start + remaining[0],
+            start * remaining[0],
+            int("".join([str(start), str(remaining[0])]))
             ]
-        )
 
-    options |= calculate_equations_two(start + remaining[0], remaining[1:])
-    options |= calculate_equations_two(start * remaining[0], remaining[1:])
-    options |= calculate_equations_two(
-        int("".join([str(start), str(remaining[0])])), remaining[1:]
-    )
+    options += calculate_equations_two(start + remaining[0], remaining[1:])
+    options += calculate_equations_two(start * remaining[0], remaining[1:])
+    options += calculate_equations_two(int("".join([str(start), str(remaining[0])])), remaining[1:])
+
 
     return options
-
 
 def part_one(file_path: str):
     """[summary]
@@ -51,7 +46,7 @@ def part_one(file_path: str):
         target, values = line.split(": ")
         target = int(target)
         values = list(map(int, values.split(" ")))
-        if target in calculate_equations(target, values[0], values[1:]):
+        if target in calculate_equations(values[0], values[1:]):
             total += target
 
     return total
@@ -67,7 +62,7 @@ def part_two(file_path: str):
         [type]: [description]
     """
 
-    # read file
+   # read file
     with open(file_path) as f:
         lines = f.readlines()
     total = 0
